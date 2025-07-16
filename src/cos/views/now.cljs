467871816345bca368beb65b31c4dbb41482aa90
Sunny <embodiedsunshine@proton.me>
2025-07-16 17:29:13 -0600

    added link to repository and TODO

--- src/cos/views/now.cljs
+++ src/cos/views/now.cljs
@@ -4,13 +4,20 @@
   [:div.p-4
    [:h2.text-4xl.thick "· now ·"]
 
+   ;; TODO make this into a function
    [:article.content-box.thin
     [:h3.text-3xl.thick "July " [:span.accent "2025"] "—"]
     [:ul
      {:style {:list-style-type :disc
               :padding-left "1em"}}
      [:li "Revisiting " [:a {:href "https://callflo.app/"} "CallFlo"]]
-     [:li "Prototyping Stamps"]]]
+     [:li "Prototyping Stamps"]
+     [:li "Got this website online ("
+      [:a {:href "https://github.com/Sunonymous/cadenceOfSun"
+           :target "_blank"
+           :rel "noopener noreferrer"}
+       "thanks GitHub!"]
+      ")"]]]
    [:article.content-box.thin
     [:h3.text-3xl.thick "June " [:span.accent "2025"] "—"]
     [:ul
