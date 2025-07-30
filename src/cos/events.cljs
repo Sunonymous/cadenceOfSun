2d6734d9251263e0b46d411bf46d081b56704f6b
Sunny <embodiedsunshine@proton.me>
2025-07-30 14:01:12 -0600

    add persistence to db

--- src/cos/events.cljs
+++ src/cos/events.cljs
@@ -1,13 +1,36 @@
 (ns cos.events
   (:require
+   [cos.db        :as db]
+   [keer.db       :as keer-db]
    [re-frame.core :as re-frame]
-   [tools.util :refer [inc-wrap-index]]
-   [cos.db :as db]
-   [keer.db :as keer-db]
-   [kitchen.db :as kitchen-db]
+   [pantry.db     :as pantry-db]
+   [kitchen.db    :as kitchen-db]
+   [cljs.reader :refer [read-string]]
    ))
 
-(re-frame/reg-event-db ::initialize-db  (constantly (merge keer-db/default-db    ;; Add DB for Keer Tool
-                                                           kitchen-db/default-db ;; Add DB Kitchen Tool
-                                                           db/default-db)))      ;; Home DB
+(def localStorage-key "cos-db")
 
+(defn db->local-storage
+  [db]
+  (js/localStorage.setItem localStorage-key (pr-str (dissoc db :current-route))))
+
+(re-frame/reg-cofx
+ :localStorage
+ (fn [coeffects storage-key]
+   (assoc coeffects :localStorage
+          (js/localStorage.getItem storage-key))))
+
+(def ->localStorage (re-frame/after db->local-storage))
+
+(re-frame/reg-event-fx
+ ::initialize-db
+ [(re-frame/inject-cofx :localStorage localStorage-key)]
+ (fn [cofx _]
+   (let [persisted-db (read-string (:localStorage cofx))]
+     ;; TODO would be better to use a spec...
+     (assoc cofx :db (if persisted-db
+                        (merge db/default-db persisted-db)
+                        (merge keer-db/default-db    ;; Add DB for Keer Tool
+                               pantry.db/default-db  ;; Add DB for Pantry Tool
+                               kitchen-db/default-db ;; Add DB for Kitchen Tool
+                               db/default-db))))))   ;; Home DB
