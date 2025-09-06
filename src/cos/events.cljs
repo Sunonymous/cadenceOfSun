af2c1b95ff5ecbc1e36f3c8d09d2cb47cfdd3d6a
Sunny <embodiedsunshine@proton.me>
2025-09-06 16:03:47 -0600

    reformat code

--- src/cos/events.cljs
+++ src/cos/events.cljs
@@ -1,11 +1,11 @@
 (ns cos.events
   (:require
-   [cos.db        :as db]
-   [keer.db       :as keer-db]
-   [re-frame.core :as re-frame]
-   [pantry.db     :as pantry-db]
-   [kitchen.db    :as kitchen-db]
-   [cljs.reader :refer [read-string]]
+   [cos.db           :as     db]
+   [keer.db          :as     keer-db]
+   [re-frame.core    :as     re-frame]
+   [pantry.db        :as     pantry-db]
+   [kitchen.db       :as     kitchen-db]
+   [cljs.reader      :refer [read-string]]
    ))
 
 (def localStorage-key "cos-db")
@@ -36,11 +36,12 @@
  ::initialize-db
  [(re-frame/inject-cofx :localStorage localStorage-key)]
  (fn [cofx _]
-   (let [persisted-db (read-string (:localStorage cofx))]
+   (let [persisted-db (read-string (:localStorage cofx))
+         additional-tools (merge keer-db/default-db    ;; Add DB for Keer Tool
+                                 pantry.db/default-db  ;; Add DB for Pantry Tool
+                                 kitchen-db/default-db ;; Add DB for Kitchen Tool
+                                 )]
      ;; TODO would be better to use a spec...
-     { :db (if persisted-db
-             (merge db/default-db persisted-db)
-             (merge keer-db/default-db    ;; Add DB for Keer Tool
-                    pantry.db/default-db  ;; Add DB for Pantry Tool
-                    kitchen-db/default-db ;; Add DB for Kitchen Tool
-                    db/default-db))})))   ;; Home DB
+     {:db (if persisted-db
+            (merge additional-tools db/default-db persisted-db)
+            (merge additional-tools db/default-db))}))) ;; Home DB
